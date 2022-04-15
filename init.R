6464d56cd4bbb1f27af22c35bee019588e63510f
kang20006 <82645538+kang20006@users.noreply.github.com>
2022-04-15 18:31:46 +0800

    Update init.R

--- init.R
+++ init.R
@@ -2,7 +2,9 @@
 # 
 # Example R code to install packages if not already installed 
 # 
+
 my_packages = c("leaflet", "shiny", "shinyWidgets","plotly","DT","dplyr","shinythemes","naniar","imputeTS","rgdal","randomcoloR","ggplot2") 
+install.packages(my_packages)
 install_if_missing = function(p) { 
   if (p %in% rownames(installed.packages()) == FALSE) { 
     install.packages(p) 
